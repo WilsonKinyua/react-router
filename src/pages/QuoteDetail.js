6cd243915463a0ac174f8c903c7ab2f05c05bf6e
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-05-04 22:41:58 +0300

    displaying comments url

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
 import Highlighted from "../components/quotes/HighlightedQuote";
@@ -28,6 +28,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <Highlighted text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            View Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
